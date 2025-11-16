57c88d52931d78377ffb6f2385d22be3c181e518
daniel-ciupek <danter005@o2.pl>
2025-11-16 20:28:14 +0100

    kod JS validacja pól formularza email i password form field

--- form_field.js
+++ form_field.js
@@ -15,12 +15,17 @@ validate = () => {
 
     switch(this.type) {
         case "password":
+            if(!this.checkTextLength()) return false;
+            if(!this.checkValidPassword()) return false;
+            return true;
          break;
         case "text":
             if(!this.checkTextLength()) return false;
             return true;
          break;
         case "email":
+            if(!this.checkEmail()) return false
+            return true;
          break;
 
     }
@@ -41,6 +46,34 @@ return false;
             this.showSuccess();
             return true;
         }
+}
+
+checkEmail = () => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(re.test(this.formField.value.trim())) {
+        this.showSuccess();
+        return true;
+    }else {
+        this.showError("Wpisz prawidłowy email.");
+        return false;
+    }
+}
+checkValidPassword = () => {
+    if(!this.matchWithPasswordId) return true;
+    const matchWitch = document.querySelector(this.matchWithPasswordId);
+
+    if(this.formField.value.length > 0 
+        && this.formField.value === matchWitch.value
+    ) {
+        this.showSuccess ();
+        return true;
+    }else {
+        this.showError("Hasła muszą się zgadzać");
+        return false;
+
+    }
+
+
 }
 showError = (msg) => {
     this.errorMsgEl.innerHTML = msg;
