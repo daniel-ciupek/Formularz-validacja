d9a3c5053c2a95b81753d754a6ab806fc579dffb
daniel-ciupek <danter005@o2.pl>
2025-11-16 18:20:23 +0100

    Kod Js validacji pól formularza typu email i password w form field & css

--- form_field.js
+++ form_field.js
@@ -7,7 +7,58 @@ class FormField {
     this.type = this.formField.type;
     this.minlength = minlength;
     this.maxlength = maxlength;
+    if(!errorMsgSelector) errorMsgSelector = `${formFieldSelector} + span`;
     this.errorMsgEl = document.querySelector(errorMsgSelector);
     this.matchWithPasswordId = matchWithPasswordId;
   }
+validate = () => {
+
+    switch(this.type) {
+        case "password":
+         break;
+        case "text":
+            if(!this.checkTextLength()) return false;
+            return true;
+         break;
+        case "email":
+         break;
+
+    }
+
+    return false;
+}
+
+checkTextLength = () => {
+    if(this.formField.value.length < this.minlength) {
+this.showError(`Wymagane minimum znaków: ${this.minlength}`);
+return false;
+
+    }else
+        if(this.formField.value.length > this.maxlength) {
+           this.showError(`Maksymalnie można użyć ${this.maxlength} znaków.`) 
+           return false;
+        }else {
+            this.showSuccess();
+            return true;
+        }
+}
+showError = (msg) => {
+    this.errorMsgEl.innerHTML = msg;
+    this.errorMsgEl.classList.add("error");
+    this.formField.classList.add("error");
+    this.errorMsgEl.classList.remove("success");
+    this.formField.classList.remove("success");
+
+}
+
+showSuccess = () => {
+    this.errorMsgEl.innerHTML = "";
+    this.errorMsgEl.classList.remove("error");
+    this.formField.classList.remove("error");
+
+    this.errorMsgEl.classList.add("success");
+    this.formField.classList.add("success");
+
+}
+
 }
